da82420ed5e7a00905a6b6b0e23637d10eda32ad
el1ght <el1ghtifyoucan@gmail.com>
2024-09-29 19:08:37 +0300

    useLoadSongUrl finished

--- components/Player.tsx
+++ components/Player.tsx
@@ -2,10 +2,13 @@
 
 import usePlayer from "@/hooks/usePlayer";
 import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 
 const Player = () => {
     const player = usePlayer();
     const { song } = useGetSongById(player.activeId);
+
+    const songUrl = useLoadSongUrl(song!);
 
 
 
